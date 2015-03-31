093d0a5af0e401322f382c3b41b8c9ce71fbbe49
Armando Amador <arm.amador@gmail.com>
2015-03-31 10:13:41 -0400

    refactor string calculator
     - Use map with proc
     - Use a, e variables as parameters for reduce (accumulator, element)

--- string_calculator/lib/string_calculator.rb
+++ string_calculator/lib/string_calculator.rb
@@ -1,14 +1,13 @@
 # Create a simple string calculator with a method int Add(string numbers)
 # The method can take 0, 1 or 2 numbers, and will return their sum
-# (for an empty string it will return 0) for example “” or “1” or “1,2”
-
+# (for an empty string it will return 0) for example "" or "1" or "1,2"
 class StringCalculator
   def self.add(input)
     if input.size == 0
       0
     else
-      numbers = input.split(',').map { |num| num.to_i }
-      numbers.inject(0) { |sum, number| sum + number.to_i }
+      numbers = input.split(',').map(&:to_i)
+      numbers.reduce(0) { |a, e| a + e.to_i }
     end
   end
 end
